dcca850f051176f07b6db86a800f933542fa9393
Eduardo Abichabki <edu.abichabki@gmail.com>
2024-10-21 10:30:15 -0300

    Bot para Whatsapp - numero em lista e numero em arq txt

---
+++ app_tel_em_lista.py
@@ -0,0 +1,34 @@
+import webbrowser
+import pyautogui
+import pyperclip
+from time import sleep
+
+def escrever_mensagem(texto):
+    pyperclip.copy(texto)
+    pyautogui.hotkey("ctrl", "v")
+
+telefones = [16997501680]
+
+for telefone in telefones:
+    link = f"https://wa.me/{telefone}"
+    webbrowser.open_new_tab(link)
+    sleep(5)
+
+    #Pressionar "Iniciar a conversa"
+    pyautogui.leftClick(1513,392, duration=1)
+    sleep(5)
+
+    #Pressionar pra utilizar o whatsapp web
+    pyautogui.leftClick(1509,451, duration=1)
+    sleep(30)
+
+    #Pressionar no local pra escrever a mensagem
+    pyautogui.leftClick(1842,968, duration=1)
+    sleep(5)
+
+    #Escrever a mensagem
+    escrever_mensagem("Ola, como voce esta")
+    sleep(5)
+
+    #Pressionar enter
+    pyautogui.press("enter")
